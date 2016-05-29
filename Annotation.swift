e0c33a256d05b1e593d8d3d8aee7d40bd3601031
Gregg Mojica <gwmojica@gmail.com>
2016-05-29 09:21:03 -0400

    add Annotation.swift

---
+++ Annotation.swift
@@ -0,0 +1,28 @@
+//
+//  Annotation.swift
+//
+//  Created by Gregg Mojica on 4/13/15.
+//  Copyright (c) 2015 Gregg Mojica. All rights reserved.
+//
+
+
+import UIKit
+import MapKit
+
+class Annotation: NSObject, MKAnnotation {
+    var title: String?
+    var locationTitle: String
+    var coordinate: CLLocationCoordinate2D
+    
+    init(title: String, locationName: String, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.locationTitle = locationName
+        self.coordinate = coordinate
+        
+        super.init()
+    }
+    
+    var subtitle: String? {
+        return locationTitle
+    }
+}
